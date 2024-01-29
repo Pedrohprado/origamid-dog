ae283521ba991dc536fc47c126b19481120a79cd
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-01-29 09:12:22 -0300

    add scroll infinity

--- src/Page/User/User.jsx
+++ src/Page/User/User.jsx
@@ -4,13 +4,17 @@ import UserHeader from './UserHeader';
 import Feed from '../../Components/Feed/Feed';
 import UserPhotoPost from './UserPhotoPost';
 import UserStats from './UserStats';
+import React from 'react';
+import { UserContext } from '../../Context/userContext';
 
 const User = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <div className=' w-5/6 flex flex-col items-center justify-center pt-20'>
       <UserHeader />
       <Routes>
-        <Route path='/' element={<Feed />} />
+        <Route path='/' element={<Feed user={data.id} />} />
         <Route path='postar' element={<UserPhotoPost />} />
         <Route path='estatisticas' element={<UserStats />} />
       </Routes>
