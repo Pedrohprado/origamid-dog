dc399e313c8a9843e4de576c8659491a0aeb8995
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-01-30 15:56:04 -0300

    test

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
-import Logo from '../Assets/dogs.svg/?react';
-import LoginLogo from '../Assets/usuario.svg/?react';
+// import Logo from '../Assets/dogs.svg/?react';
+// import LoginLogo from '../Assets/usuario.svg/?react';
 import React from 'react';
 import { UserContext } from '../Context/userContext';
 
@@ -10,12 +10,12 @@ const Header = () => {
     <header className=' h-16 w-svw flex items-center justify-center shadow-md fixed z-10 bg-white top-0'>
       <nav className='  w-full px-2 flex items-center justify-between '>
         <Link to='/' className='px-5 py-2' aria-label='Dogs | Home'>
-          <Logo className=' text-stone-800' />
+          {/* <Logo className=' text-stone-800' /> */}
         </Link>
         {data !== null ? (
           <button className=' px-5 py-2 text-stone-800 font-medium transition-1 flex items-center justify-center gap-2'>
             {data.nome}
-            <LoginLogo className=' text-stone-800' />
+            {/* <LoginLogo className=' text-stone-800' /> */}
           </button>
         ) : (
           <Link
@@ -23,7 +23,7 @@ const Header = () => {
             className=' px-5 py-2 text-stone-800 font-medium transition-1 flex items-center justify-center gap-2'
           >
             Login | Criar
-            <LoginLogo className=' text-stone-800' />
+            {/* <LoginLogo className=' text-stone-800' /> */}
           </Link>
         )}
       </nav>
