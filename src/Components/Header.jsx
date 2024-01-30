3f634048a36ffda6954adc188e8c1919f39ca8e6
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-01-30 13:43:31 -0300

    test

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     <header className=' h-16 w-svw flex items-center justify-center shadow-md fixed z-10 bg-white top-0'>
       <nav className='  w-full px-2 flex items-center justify-between '>
         <Link to='/' className='px-5 py-2' aria-label='Dogs | Home'>
-          <Dog />
+          <Dog className=' text-stone-800' />
         </Link>
         {data !== null ? (
           <button className=' px-5 py-2 text-stone-800 font-medium transition-1 flex items-center justify-center gap-2'>
