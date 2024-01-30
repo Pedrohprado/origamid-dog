f2f1c3c00aa9862398b58866903891f807b4401c
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-01-30 14:33:15 -0300

    test

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import Dog from '../Assets/dogs.svg/?react';
+import Logo from '../Assets/dogs.svg/?react';
 import Login from '../Assets/usuario.svg/?react';
 import React from 'react';
 import { UserContext } from '../Context/userContext';
@@ -10,7 +10,7 @@ const Header = () => {
     <header className=' h-16 w-svw flex items-center justify-center shadow-md fixed z-10 bg-white top-0'>
       <nav className='  w-full px-2 flex items-center justify-between '>
         <Link to='/' className='px-5 py-2' aria-label='Dogs | Home'>
-          <Dog className=' text-stone-800' />
+          <Logo className=' text-stone-800' />
         </Link>
         {data !== null ? (
           <button className=' px-5 py-2 text-stone-800 font-medium transition-1 flex items-center justify-center gap-2'>
