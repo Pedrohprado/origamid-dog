b1f160a46dc2a5234cf9620d8e91f0da0a6a0ba4
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-01-31 08:06:32 -0300

    teste SVG

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LogoMarca from '../Assets/dogs.svg?react';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../Context/userContext';
 
@@ -8,12 +9,13 @@ const Header = () => {
     <header className=' h-16 w-svw flex items-center justify-center shadow-md fixed z-10 bg-white top-0'>
       <nav className='  w-full px-2 flex items-center justify-between '>
         <Link to='/' className='px-5 py-2' aria-label='Dogs | Home'>
-          <img src='src\Assets\dogs.svg' alt='cachorro' />
+          {/* <img src='src\Assets\dogs.svg' alt='cachorro' /> */}
+          <LogoMarca />
         </Link>
         {data !== null ? (
           <button className=' px-5 py-2 text-stone-800 font-medium transition-1 flex items-center justify-center gap-2'>
             {data.nome}
-            <img src='src\Assets\usuario.svg' alt='usuario' />
+            <img src='../Assets/usuario.svg' alt='usuario' />
           </button>
         ) : (
           <Link
