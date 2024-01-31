2474667b00331298df12dbde07825cab0a1b0eee
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-01-31 08:33:30 -0300

    cahnges

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,14 +10,16 @@ const Header = () => {
     <header className=' h-16 w-svw flex items-center justify-center shadow-md fixed z-10 bg-white top-0'>
       <nav className='  w-full px-2 flex items-center justify-between '>
         <Link to='/' className='px-5 py-2' aria-label='Dogs | Home'>
-          {/* <img src='src\Assets\dogs.svg' alt='cachorro' /> */}
           <LogoMarca />
         </Link>
         {data !== null ? (
-          <button className=' px-5 py-2 text-stone-800 font-medium transition-1 flex items-center justify-center gap-2'>
+          <Link
+            to='/conta'
+            className=' px-5 py-2 text-stone-800 font-medium transition-1 flex items-center justify-center gap-2'
+          >
             {data.nome}
             <User />
-          </button>
+          </Link>
         ) : (
           <Link
             to='login'
